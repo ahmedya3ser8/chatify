28638ad9c1b061786981a0ffdc235b6de6749f6d
ahmed yasser <ahmedya3ser8@gmail.com>
2026-05-06 13:04:01 +0300

    feat: add baseURL

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: 'https://chatify-app-v1.up.railway.app/api',
+  baseURL: 'https://chatify-v1.vercel.app/api',
   withCredentials: true
 });
